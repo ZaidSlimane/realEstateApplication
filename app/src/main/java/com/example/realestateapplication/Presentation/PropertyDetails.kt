ef2953d5eedece28991a3354ecbbb8a775a89f5b
Zaid Slimane <102682510+ZaidSlimane@users.noreply.github.com>
2023-12-27 01:11:26 -0800

    PropertyDetails edit

--- app/src/main/java/com/example/realestateapplication/Presentation/PropertyDetails.kt
+++ app/src/main/java/com/example/realestateapplication/Presentation/PropertyDetails.kt
@@ -1,5 +1,6 @@
 package com.example.realestateapplication.Presentation
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -21,9 +22,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,9 +35,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -51,11 +56,11 @@ fun PropertyDetailsScreen (){
             .fillMaxHeight(0.5f)){
 
 
-        Image(painter = painterResource(id = R.drawable.img_1) ,
-            contentDescription = "",
-            modifier = Modifier
-                .fillMaxSize(),
-            contentScale = ContentScale.FillHeight)
+            Image(painter = painterResource(id = R.drawable.img_1) ,
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentScale = ContentScale.FillHeight)
         }
 
         Box(
@@ -70,94 +75,140 @@ fun PropertyDetailsScreen (){
             Card( modifier = Modifier
                 .fillMaxSize(),
                 colors = CardDefaults.cardColors(
-                    containerColor = Color.White)
-                ){
+                    containerColor = Color.White),
+            )
+            {
 
 
-                Row(horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier
-                        .offset(x = (20).dp)
-                        .fillMaxWidth()
-                        .padding(
-                            start = 16.dp,
-                            top = 16.dp
-                        ))
-                {
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(SweepGradientExample()) ){
 
 
-                    Text(text = "Villa Vice City",
-                        style = TextStyle(
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold,
-                        ),
-                        modifier = Modifier.padding()
-                    )
+                    GradientCircularShape()
 
 
-                    Card(modifier = Modifier
-                        .border(
-                            BorderStroke(1.dp, Color.Black),
-                            shape = RoundedCornerShape(50.dp)
-                        )
-                        .width(120.dp)
-                        .height(50.dp),
-                        colors = CardDefaults.cardColors(
-                            containerColor = Color.White),
-                        shape = RoundedCornerShape(50.dp),
-                        elevation = CardDefaults.cardElevation(
-                            defaultElevation = 5.dp,
-                            focusedElevation = 2.dp
-                        )
 
-                        ){
-                        Column(
-                            modifier = Modifier.fillMaxSize(),
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(text = "26700$")
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(start = 16.dp)) {
+
+                        Row(horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier
+                                .offset(x = (20).dp)
+                                .fillMaxWidth()
+                                .padding(
+                                    top = 16.dp
+                                ))
+                        {
+
+
+                            Text(text = "Villa Vice City",
+                                style = TextStyle(
+                                    fontSize = 24.sp,
+                                    fontWeight = FontWeight.Bold,
+                                ),
+                                modifier = Modifier
+                                    .padding()
+                                    .offset(-20.dp)
+                            )
+
+
+
+                            Card(modifier = Modifier
+                                .border(
+                                    BorderStroke(1.dp, Color.Black),
+                                    shape = RoundedCornerShape(50.dp)
+                                )
+                                .width(120.dp)
+                                .height(50.dp),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = Color.White),
+                                shape = RoundedCornerShape(50.dp),
+                                elevation = CardDefaults.cardElevation(
+                                    defaultElevation = 5.dp,
+                                    focusedElevation = 2.dp
+                                )
+
+                            ){
+                                Column(
+                                    modifier = Modifier.fillMaxSize(),
+                                    verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally) {
+                                    Text(text = "26700$")
+                                }
+                            }
                         }
+
+
+
+                        Divider(color = Color.Gray,
+                            thickness = 1.dp,
+                            modifier = Modifier
+                                .padding(top = 16.dp,
+                                    end = 16.dp))
+
+                        PropertySpecs()
+
+                        Divider(color = Color.Gray,
+                            thickness = 1.dp,
+                            modifier = Modifier
+                                .padding(
+                                    end = 16.dp))
+
+                        Column(Modifier.padding(top = 16.dp)) {
+                            Text("Description",
+                                style = TextStyle(
+                                    fontSize = 24.sp,
+                                    fontWeight = FontWeight.Bold,
+                                ),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(bottom = 8.dp)
+                            )
+
+                            Text("Located in the heart of Anytown, this home boasts a spacious living room with large windows that let in plenty of natural light.")
+
+                            Box(modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                                .padding(bottom = 16.dp)
+
+                            ){
+                                Button(onClick = { /*TODO*/ },
+                                    colors = ButtonDefaults.buttonColors(Color(0xFF001F3F)),
+                                    modifier = Modifier
+                                        .align(Alignment.BottomCenter)
+                                        .width(166.dp)
+                                        .height(63.dp)
+                                        .background(
+                                            color = Color(0xFF001F3F),
+                                            shape = RoundedCornerShape(size = 10.dp),
+                                        )
+                                ) {
+                                    Text(text = "Buy")
+                                }
+                            }
+
+                        }
+
                     }
+
+
+
+
                 }
-
-
-
-                Divider(color = Color.Gray,
-                    thickness = 1.dp,
-                    modifier = Modifier
-                        .padding(top = 24.dp,
-                            end = 16.dp,
-                            start = 16.dp),)
-
-                PropertySpecs()
-
-                Divider(color = Color.Gray,
-                    thickness = 1.dp,
-                    modifier = Modifier
-                        .padding(
-                            end = 16.dp,
-                            start = 16.dp),)
-
-                Column {
-                    Text("Description",
-                        style = TextStyle(
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold,
-                        )
-                    )
-
-
-                    Text("Located in the heart of Anytown, this home boasts a spacious living room with large windows that let in plenty of natural light.")
-                }
-
-
-
-
 
 
             }
 
-        }    }
+
+        }
+    }
 }
+
+
 
 @Composable
 fun PropertySpecs(){
@@ -170,7 +221,7 @@ fun PropertySpecs(){
                 vertical = 16.dp,
                 horizontal = 16.dp
             ),
-        horizontalArrangement = Arrangement.SpaceEvenly
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
 
         CircularOutlinedBox(icon = R.drawable.search)
@@ -182,7 +233,7 @@ fun PropertySpecs(){
 
 
     }
-    }
+}
 
 
 @Composable
@@ -215,4 +266,59 @@ fun CircularOutlinedBox(icon : Int){
 
     }
 
+}
+
+@SuppressLint("InvalidColorHexValue")
+@Composable
+fun SweepGradientExample() : Brush {
+    val colorStops = listOf(
+        0.02f to Color(0x20B9DAFB),
+        0.14f to Color(0x20ECACAD),
+        0.31f to Color(0x309895EE),
+        0.54f to Color(0x404490A2F8),
+        0.81f to Color(0xFFC55492),
+    ).toTypedArray()
+
+    val density = LocalDensity.current
+    val centerX: Float
+    val centerY: Float
+    with(density) {
+        centerX =-300.dp.toPx() /2
+        centerY = 0.dp.toPx() / 2
+    }
+    val brush = Brush.sweepGradient(
+        colorStops = colorStops,
+        center = Offset(centerX, centerY)
+    )
+    return brush
+}
+
+@Composable
+fun GradientCircularShape() {
+
+
+
+    val density = LocalDensity.current
+    val centerX: Float
+    val centerY: Float
+    with(density) {
+        centerX =300.dp.toPx() /2
+        centerY = 0.dp.toPx() / 2
+    }
+
+    val brush = Brush.sweepGradient(
+        colors = listOf(
+            Color(0x10F4AF4),
+            Color(0x209895EE)
+        ),
+        center = Offset(centerX, centerY)
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxSize()
+            .size(30.dp)
+            .background(brush)
+    ) {
+    }
 }
